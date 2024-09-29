1b96da48160674ac1da49cb2818b94f10ebb8844
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2024-09-29 23:52:18 +0600

    fix: fix job display

--- freelance-bangladesh-client/components/jobs/jobList.js
+++ freelance-bangladesh-client/components/jobs/jobList.js
@@ -8,7 +8,7 @@ const JobList = ({ jobs, total }) => {
         <h1 className="text-3xl font-bold text-center my-8">Available Jobs: {total}</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {jobs && jobs.map((job) => (
-            <JobCard key={job.id} job={job} />
+            <JobCard key={job.ID} job={job} />
             ))}
         </div>
     </div>
