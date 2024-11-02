9035e7b7f01f24f930b7cf84782e68c016ef562a
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2024-11-02 20:05:49 +0600

    chore: use proper envs in frontend docker image

--- scripts/setenv.example.sh
+++ scripts/setenv.example.sh
@@ -1,19 +1,31 @@
+## commented out envs are not necessary to run all containers in a single host server
+
+## keycloak envs
 export KEYCLOAK_DATABASE_USER=xxx
 export KEYCLOAK_DATABASE_PASSWORD=xxx
 export KEYCLOAK_DATABASE_NAME=keycloak-db
 export KEYCLOAK_ADMIN=xxx
 export KEYCLOAK_ADMIN_PASSWORD=xxx
 
+## database envs
 export PGADMIN_EMAIL=xxx
 export PGADMIN_PASSWORD=xxx
 
-export API_KEYCLOAK_BASEURL=http://127.0.0.1:9990 # only for running locally
-export API_KEYCLOAK_REALM=freelance-bangladesh
+# export API_KEYCLOAK_BASEURL=http://127.0.0.1:9990 # only for running locally
+# export API_KEYCLOAK_REALM=freelance-bangladesh # only for running locally
 export API_KEYCLOAK_RESTAPI_CLIENTID=backend-api
 export API_KEYCLOAK_RESTAPI_CLIENTSECRET=xxx
 
-export API_DATABASE_HOST=127.0.0.1 # only for running locally
-export API_DATABASE_PORT=5432 # only for running locally
+# export API_DATABASE_HOST=127.0.0.1 # only for running locally
+# export API_DATABASE_PORT=5432 # only for running locally
 export API_DATABASE_USER=admin
 export API_DATABASE_PASSWORD=xxx
 export API_DATABASE_NAME=freelance-db
+
+## frontend envs
+# export API_URL=http://localhost:5000 # only for running locally
+# export NEXTAUTH_URL=http://localhost:3000 # only for running locally
+export NEXTAUTH_SECRET=any-random-long-string-123
+export KEYCLOAK_CLIENT_ID=frontend-client
+export KEYCLOAK_CLIENT_SECRET=JJxzZNg0aPNDbftzYHu7bjAsWeN6Mbov
+# export KEYCLOAK_ISSUER=http://localhost:9990/realms/freelance-bangladesh # only for running locally
